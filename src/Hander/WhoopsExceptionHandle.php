e849643510db27d8468ad262795ded57603ce520
xiaodit <saybabi@163.com>
2019-07-24 16:03:16 +0800

    update

--- src/Hander/WhoopsExceptionHandle.php
+++ src/Hander/WhoopsExceptionHandle.php
@@ -1,14 +1,29 @@
 <?php
+declare(strict_types = 1);
+
 namespace think\Whoops\Hander;
 
 use think\exception\Handle;
 use think\exception\HttpException;
 use think\exception\ValidateException;
 use think\Response;
+use think\App;
+
 use Throwable;
+use Whoops\Run;
+use think\Whoops\Whoops;
 
 class WhoopsExceptionHandle extends Handle
 {
+    private $whoops;
+
+    public function __construct(App $app, Run $run)
+    {
+        parent::__construct($app);
+
+        $this->whoops = new Whoops($run);
+    }
+
     public function render($request, Throwable $e): Response
     {
         // 参数验证错误
@@ -16,28 +31,23 @@ class WhoopsExceptionHandle extends Handle
             return json($e->getError(), 422);
         }
 
-        // 请求异常
-        if ($e instanceof HttpException && $request->isAjax()) {
-            return response($e->getMessage(), $e->getStatusCode());
-        }
-
         if (env('APP_DEBUG')) {
-            return $this->renderExceptionWithWhoops($e);
+            
+            // 请求异常
+            if ($e instanceof HttpException && $request->isAjax()) {
+                $content = $this->whoops->renderJsonException($e);
+            } else {
+                $content = $this->whoops->renderException($e);
+            }
+
+            return Response::create(
+                $content,
+                $e->getStatusCode(),
+                $e->getHeaders()
+            );
         }
 
         // 其他错误交给系统处理
         return parent::render($request, $e);
-    }
-
-    protected function renderExceptionWithWhoops(Throwable $e): Response
-    {
-        $whoops = new \Whoops\Run;
-        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
-
-        return Response::create(
-            $whoops->handleException($e),
-            $e->getStatusCode(),
-            $e->getHeaders()
-        );
     }
 }
---
+++ src/Whoops.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types = 1);
+
+namespace think\Whoops;
+
+use Throwable;
+use Whoops\Run;
+use think\Response;
+use Whoops\Handler\JsonResponseHandler;
+use Whoops\Handler\PrettyPageHandler;
+
+class Whoops
+{
+    private $run;
+
+    public function __construct(Run $run)
+    {
+        $this->run = $run;
+        $this->run->register();
+    }
+
+    public function renderException(Throwable $e): String
+    {
+        $this->run->pushHandler(new PrettyPageHandler());
+        return $this->run->handleException($e);
+    }
+
+    public function renderJsonException(Throwable $e): String
+    {
+        $this->run->pushHandler(new JsonResponseHandler());
+        return $this->run->handleException($e);
+    }
+}
